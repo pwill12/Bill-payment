7c1840fd629f6e5e19326dcef655688b69cadd52
pwill12 <princesolo919@gmail.com>
2025-07-16 16:38:21 +0100

    get user

--- client/components/Balance.tsx
+++ client/components/Balance.tsx
@@ -1,31 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Feather } from '@expo/vector-icons'
-import AddMoney from './AddMoney'
+import { View, Text } from "react-native";
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import AddMoney from "./AddMoney";
+import { useCurrentUser } from "@/hooks/useCurrentuser";
 
 const Balance = () => {
+  const { currentUser, isLoading } = useCurrentUser();
+  console.log(currentUser);
   return (
     <View className="px-5 py-5 bg-green-500 rounded-2xl justify-center">
-          <View className="flex-row justify-between mb-3">
-            <View className="flex-row items-center gap-2">
-              <Feather name="shield" color={"white"} />
-              <Text className="color-white">Available Balance</Text>
-              <Feather name="eye" color={'white'}/>
-            </View>
-            <View className="flex-row items-center gap-2">
-              <Text className="color-slate-100">Transaction history</Text>
-              <Feather name="arrow-right" size={10} color={'#f1f5f9'}/>
-            </View>
-          </View>
-          <View className="flex-row justify-between">
-            <View className="flex-row items-center">
-              <Feather name="dollar-sign" size={23} color={"white"} />
-              <Text className="color-white font-medium text-3xl">200</Text>
-            </View>
-            <AddMoney/>
-          </View>
+      <View className="flex-row justify-between mb-3">
+        <View className="flex-row items-center gap-2">
+          <Feather name="shield" color={"white"} />
+          <Text className="color-white">Available Balance</Text>
+          <Feather name="eye" color={"white"} />
         </View>
-  )
-}
+        <View className="flex-row items-center gap-2">
+          <Text className="color-slate-100">Transaction history</Text>
+          <Feather name="arrow-right" size={10} color={"#f1f5f9"} />
+        </View>
+      </View>
+      <View className="flex-row justify-between">
+        <View className="flex-row items-center">
+          <Feather name="dollar-sign" size={23} color={"white"} />
+          <Text className="color-white font-medium text-3xl">200</Text>
+        </View>
+        <AddMoney />
+      </View>
+    </View>
+  );
+};
 
-export default Balance
+export default Balance;
