16fe0d28f0d001f92f059c0323db913412795ca7
pwill12 <princesolo919@gmail.com>
2025-07-27 21:23:07 +0100

    transfer updated

--- backend/src/controllers/transactions.js
+++ backend/src/controllers/transactions.js
@@ -1,8 +1,9 @@
+import { getAuth } from "@clerk/express";
 import { sqldb } from "../config/db.js";
 
 export async function transactionsTable() {
     try {
-        await sqldb`CREATE TABLE IF NOT EXISTS transactions(
+        await sqldb`CREATE TABLE IF NOT EXISTS transactionlog(
                 id SERIAL PRIMARY KEY,
                 type VARCHAR(30) NOT NULL,
                 amount DECIMAL(20,2) NOT NULL,
@@ -10,7 +11,7 @@ export async function transactionsTable() {
                 receiver VARCHAR(20) NULL,
                 created_at DATE NOT NULL DEFAULT CURRENT_DATE
                 )`;
-        console.log("transactions table created successfully");
+        console.log("transaction table created successfully");
     } catch (error) {
         console.error("error creating transactions table", error);
         process.exit(1)
@@ -22,63 +23,43 @@ export async function transactions(req, res) {
         await transactionsTable();
 
         const { amount, type, sender, receiver } = req.body
-        const { user_id } = req.params
+        const { userId } = getAuth(req)
 
         const getbalance = await sqldb`
-            SELECT balance FROM users WHERE user_id = ${user_id}
+            SELECT * FROM users WHERE clerk_id = ${userId}
         `
         const senderbal = parseFloat(getbalance[0].balance)
 
-        const getreceiverbalance = await sqldb`
-            SELECT balance FROM users WHERE user_id = ${receiver}
+        const getreceiver = await sqldb`
+            SELECT * FROM users WHERE username = ${receiver}
         `
-        const receiverbal = parseFloat(getreceiverbalance[0].balance)
+        const receiverbal = parseFloat(getreceiver[0].balance)
 
         if (senderbal <= 0 || amount <= 0) {
             console.log("balance is less than 0")
             return res.status(400).json({ message: "all fields required" })
         }
 
-        if (type == "send") {
-            const updated_receiver_balance = receiverbal + amount
-
-            const updated_user_balance = senderbal - amount
-
-            console.log(amount)
-
-            try {
-                await sqldb`
-                UPDATE users
-                SET balance = ${updated_user_balance}
-                WHERE user_id = ${user_id}
-                RETURNING *
+        console.log(amount)
+        try {
+            const transaction = await sqldb`
+                BEGIN TRANSACTION;
+                UPDATE users SET balance = balance - ${senderbal} WHERE clerk_id = ${userId};
+                UPDATE users SET balance = balance + ${receiverbal} WHERE clerk_id = ${receiver};
+                INSERT INTO transactionLog (sender, receiver, type , amount)
+                VALUES ${getbalance[0].username, receiver, type, amount};
+                ROLLBACK;
+                COMMIT;
             `
-                await sqldb`
-                    UPDATE users
-                    SET balance = ${updated_receiver_balance}
-                    WHERE user_id = ${receiver}
-                    RETURNING *
-                `
-                // res.status(201).json([updated_sender[0], receiver_acct[0]])
+            res.status(201).json(transaction)
 
-            } catch (error) {
-                res.status(401).json({ message: "error creating transaction sending" })
-                console.log("error sending money", error)
-            }
-
+        } catch (error) {
+            res.status(401).json({ message: "error creating transaction sending" })
+            console.log("error sending money", error)
         }
-
-        const transaction = await sqldb`
-            INSERT INTO transactions(type, amount, sender, receiver)
-            VALUES (${type}, ${amount}, ${sender}, ${receiver})
-            RETURNING *
-        `
-
-        res.status(201).json(transaction)
-
 
     } catch (error) {
         console.log(error)
-        res.status(500).json({message: "internal server error"})
+        res.status(500).json({ message: "internal server error" })
     }
 }
