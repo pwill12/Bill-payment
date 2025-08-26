a9cfbc5dfd6f48cb1d72edd7a0b1f271f8ee33de
pwill12 <princesolo919@gmail.com>
2025-08-26 19:51:08 +0100

    api modified

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -324,7 +324,7 @@ export async function ValidateCustomer(req, res) {
             message: error?.message,
         });
         return res.status(status).json({
-            message: "validation failed",
+            message: "validation fail",
             providers: error?.response?.message,
             mssg: error
         })
